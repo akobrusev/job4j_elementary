81bdd9d160b5f302b8dbcf903900d94508cc98c2
akobrusev <akobrusev@gmail.com>
2020-04-03 15:57:10 +0300

    3.3. Разбор первой программы.[#267255]

---
+++ src/ru/jo4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.jo4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Anotn kobrusev");
+        System.out.println("03.04.2020");
+    }
+}
