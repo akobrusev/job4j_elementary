4d03adbce37d67a3dc44317485eb7289bb63b32f
akobrusev <akobrusev@gmail.com>
2020-04-15 21:39:29 +0300

    6.0.2. Заполнение массива.[#267224]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,8 +4,14 @@ import javax.print.DocFlavor;
 
 public class ArrayDefinition {
     public static void main(String [] args) {
-        short [] ages = new short[10];
-        String [] surname = new String[100500];
-        float [] prices = new float[40];
+        String [] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Anton Kobrusev";
+        names[2] = "Dmitiy Moiseev";
+        names[3] = "Mark Kobrusev";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
