1fef1395f5337f41ac24ac66b6df655494e3a33f
akobrusev <akobrusev@gmail.com>
2020-06-01 20:37:07 +0300

    1. Конструктор по умолчанию.[173889#267383]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -6,43 +6,7 @@ public class Item {
         super();
         System.out.println("load item");
     }
-    public static class Bug extends Item {
-    }
     public static void main(String[] args) {
         Bug bug = new Bug();
-    }
- }
- class Animal {
-    public Animal() {
-        super();
-        System.out.println ("Animal");
-    }
-    public static class Name extends Animal {
-    }
-     public static void main(String[] args) {
-         Name name = new Name();
-     }
- }
-class Predator {
-    public Predator() {
-        super();
-        System.out.println ("Predator");
-    }
-    public static class Name extends Predator {
-    }
-    public static void main(String[] args) {
-        Name name = new Name();
-    }
-}
-
-class Tiger {
-    public Tiger() {
-        super();
-        System.out.println ("Tiger");
-    }
-    public static class Name extends Tiger {
-    }
-    public static void main(String[] args) {
-        Name name = new Name();
     }
 }
