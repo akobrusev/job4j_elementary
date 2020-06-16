86f4cc2418e02faa0575caf0ee72526ed8fbbac3
akobrusev <akobrusev@gmail.com>
2020-06-16 21:48:40 +0300

    2. Перегрузка конструктора.[173890#267384]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -2,10 +2,19 @@ package ru.job4j.tracker;
 
 public class Item {
 
+    private String id;
+
+    private String name;
+
     public Item() {
     }
-    public Item(String name) {
+
+    public Item(String id) {
     }
+
+    public Item(String id, String name) {
+    }
+
     public static void main(String[] args) {
         Item item = new Item();
     }
